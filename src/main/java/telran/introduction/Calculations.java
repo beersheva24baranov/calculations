617537e1314fa338f5e7af1405b3392a0f9967a1
AlexanderBaranov <avbc@mail.ru>
2024-07-08 14:51:08 +0300

    add max digit + isDevidedOn

--- src/main/java/telran/introduction/Calculations.java
+++ src/main/java/telran/introduction/Calculations.java
@@ -10,23 +10,45 @@ public static int multiply(int op1, int op2) {
     return res;
 }
 public static int divide(int op1, int op2) {
-     int res = op1/op2;
+    if (op2 == 0) {
+        return 0;
+       } 
+    int res = op1/op2;
      return res;
 }
 public static int subtract(int op1, int op2) {
-    //TODO
-    return -1;
+    int res = op1 - op2;
+    return res;
 }
 public static int sumOfDigits(int number) {
-    //TODO
-    return -1;
+    int res = 0;
+    while (number!=0) {
+        res += number % 10;
+        number /= 10;
+            }
+    return res;
 }
 public static int maxDigit(int number){
-    //TODO
-    return -1;
+    int maxDigit = 0;
+    number = Math.abs(number);
+    while (number !=0) {
+        int currentDigit;
+        currentDigit = number % 10;
+        if (maxDigit < currentDigit) {
+            maxDigit = currentDigit;
+        }
+        number /= 10;
+    }
+        return maxDigit;
+    }
+public static boolean isDividedOn(int number, int divider) {
+    if (divider == 0) {
+        return false;
+    }
+    if (number % divider != 0) {
+        return false;
+    }
+    return true;
 }
-public static boolean isDividedOn(int number, int dividor) {
-    //TODO
-    return false;
 }
-}
+
--- src/test/java/telran/introduction/CalculationsTest.java
+++ src/test/java/telran/introduction/CalculationsTest.java
@@ -14,4 +14,25 @@ public class CalculationsTest {
     void multiplyTest() {
         assertEquals(12, multiply(2, 6));
     }
+    @Test
+    void divideTest() {
+        assertEquals(1, divide(6, 6));
+    }
+    @Test
+    void subtracTest() {
+        assertEquals(7, subtract(13, 6));
+    }
+    @Test
+    void sumOfDigitsTest() {
+        assertEquals(9, sumOfDigits(135));
+    }
+    @Test
+void maxDigitTest() {
+    assertEquals(5, maxDigit(13235));
+     }
+     @Test
+void isDividedOnTest() {
+    assertEquals(true, isDividedOn(10, 5));
+     }
 }
+
